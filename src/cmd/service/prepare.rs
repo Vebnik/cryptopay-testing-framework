714ca3c78e513d1487fe1d2cff21e0a74047a3d5
Tommy Hash <thxsh@pm.me>
2024-05-12 13:02:46 +0300

    Minor fix

--- src/cmd/service/prepare.rs
+++ src/cmd/service/prepare.rs
@@ -18,9 +18,9 @@ use crate::{
 /// -> Check and create admin user ✅
 /// -> Check and create tester user ✅
 /// -> Check and create networks ✅
-/// -> Check and deploy tokens to core wallet
-/// -> Check and add assets to networks
-/// -> Check and create wallet for tester user
+/// -> Check and deploy tokens to core wallet ✅
+/// -> Check and add assets to networks ✅
+/// -> Check and create wallet for tester user ✅
 pub async fn exec(config: Arc<Config>) -> Result<()> {
     cmd::db::utils::check_db_exists(Arc::clone(&config)).await?;
     cmd::service::utils::check_service_exists(Arc::clone(&config)).await?;
